841bb28c5554f577c4d60f54f6e8a1f98f993243
VishnuPuneethR <93135223+VishnuPunith@users.noreply.github.com>
2024-01-10 10:33:42 +0530

    change the design

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template>
-  <Tutorial/>
+  <div>
+  <FireStote/>
+  
+  <!-- <h1>Hello This is vistas</h1> -->
+</div>
 </template>
 
 <script>
+import FireStote from '../components/FireStote.vue';
+
 export default {
-  name: 'IndexPage'
+    name: 'IndexPage',
+    components: { FireStote }
 }
 </script>
+
